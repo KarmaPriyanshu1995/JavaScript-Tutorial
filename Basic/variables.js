8b05997cfc383b81d72ebe6ac97f6d4a6232e89c
Priyanshu Singh <141661989+KarmaPriyanshu1995@users.noreply.github.com>
2024-06-05 17:08:13 +0000

    variables Decleration

---
+++ Basic/variables.js
@@ -0,0 +1,19 @@
+const accountNumber =14433887  
+//const cannot be Redeclared.
+//const cannot be Reassigned.
+//const have Block Scope.
+let accountEmail ="priyanshu@gmail.com" 
+//let have Block Scope.
+//let must be Declared before use.
+//let cannot be Redeclared in the same scope. 
+var accountPassword ="11223344"
+//let have Global Scope.
+//let must be Declared before use.
+//let can be declared in the same scope. 
+accountCity = "Jaipur"
+
+accountEmail="yar@gmail.com"
+accountPassword=90909090
+accountCity="Goa"
+
+console.table([accountEmail,accountNumber,accountPassword,accountCity])
